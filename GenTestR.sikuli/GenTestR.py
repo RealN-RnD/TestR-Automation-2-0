81727d4f9f08046b11ea260b555d1f91d3c338c2
Phildor92 <phillip.evans09@hotmail.com>
2016-08-11 09:05:24 +0200

    1.5 update, easier variable entry
    - still no support for input popup, pending new experimental (see issue #1)
    - added winVariable and macVariable. The former is by default blank and the latter needs to be expanded to include exact mac version
    - also check nhc software version
    - nhc version will be made a variable in a future release. 
    - also updates to capture images to improve accuracy
    - update the screen variable to correct screen of your choice

--- GenTestR.sikuli/GenTestR.py
+++ GenTestR.sikuli/GenTestR.py
@@ -1,7 +1,8 @@
-version = "AutomateG1.4"
+version = "AutomateG1.5"
 curScr = Screen(0)
 popat(curScr)
-otherVariable = "User Config 1.4.0.35511"
+winVariable = ""
+macVariable = "Mac OS X 10." + Key.ENTER + "MacBook Pro " + Key.ENTER + "Model"
 
 isWindows = popAsk("Are you testing on Windows?", version)
 
@@ -9,15 +10,17 @@ while True:
     curScr.click("1470125507873.png")
     popup("Press OK when you have finished testing", version)
     curScr.click("1470125519799.png")
+    curScr.wait("1470046377455.png") 
     if not isWindows:
         curScr.click("1470046377455.png")   
-        curScr.paste("Mac OS X 10.10.5" + Key.ENTER + "MacBook Pro (Retina 13-inch Late 2013)" + Key.ENTER + "Model A1502")
-    curScr.click("1470046377455.png")
-    curScr.paste(otherVariable)
+        curScr.paste(macVariable)
+    else:
+        curScr.click("1470046377455.png")
+        curScr.paste(winVariable)
     curScr.click("1470046406673.png")
     curScr.click("1470223593900.png")
     
-    curScr.paste("1.14.0.37")
+    curScr.paste("1.14.0.44")
     curScr.click("1470046187921.png")
     if not isWindows:
         curScr.type("Mac OS X 10.10")
@@ -29,9 +32,8 @@ while True:
         curScr.click("1470210216668.png")
         curScr.paste(defect)
         curScr.popup("Select test steps that failed and change Status", version)
-    wait(2)
     curScr.click("1470644657529.png")
-    wait(2)
+    wait(1)
     curScr.click("1470126608431.png")
     doWeContinue = popAsk("Another test?", version)
     if not doWeContinue:
