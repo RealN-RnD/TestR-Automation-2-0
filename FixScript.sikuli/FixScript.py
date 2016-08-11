16c6ed631d72c510bc7aeb6637c1d11ad164677a
Phildor92 <phillip.evans09@hotmail.com>
2016-08-11 09:28:05 +0200

    Script to fix test cases 1.0
    This script runs through consecutive, passed test cases and modifies them. Modify the code with the appropriate images/modifications

---
+++ FixScript.sikuli/FixScript.py
@@ -0,0 +1,9 @@
+while(True):
+    click("1470748715628.png")
+    click("1470838391585.png")
+    type(Key.BACKSPACE)
+    type("4")
+    click("1470748671956.png")
+    click("1470748681891.png")
+    wait(2)
+
